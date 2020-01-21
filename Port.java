d651594eaa7b380cbe0ad110ef585c115bec00e9
Majid Boustanifar <majidbst@gmail.com>
2020-01-21 10:47:54 +0100

    Update Port.java
    revised shipsInPort by shipsId.size()

--- Port.java
+++ Port.java
@@ -7,7 +7,6 @@ public class Port {
 	private String name;
 	private int[] position;  // Port position in the map
 	private int cargoInPort; // Total current cargo units in a port
-	private int shipsInPort; // No. of ships which already anchored in a port
 	private List<Integer> shipsId; // keeps a list of ship IDs which already located in a port
 	
 	//class constructor
@@ -15,7 +14,6 @@ public class Port {
 		
 		this.position = position;
 		this.cargoInPort = 0;
-		this.shipsInPort = 0;
 		this.shipsId = new ArrayList<Integer>();
 	}
 	
@@ -37,20 +35,18 @@ public class Port {
 	
 	// add a new anchored ship into the ship list at a port
 	public void addShipInDock(int shipId) {
-		this.shipsInPort++;
 		this.shipsId.add(shipId);
 	}
 	
 	// remove a ship from the ship list, when it lefts a port
 	public void dumpShipsFromPort(int shipId) {
-		this.shipsInPort--;
-		int removeValue = this.shipsId.indexOf(shipsId);
+		int removeValue = this.shipsId.indexOf(shipId);
 		this.shipsId.remove(removeValue);
 	}
 	
-	// Return the no. of ships which anchored in a port
+	// Return the no. of ships which anchored in a port.
 	public int getShipsInPort() {
-		return this.shipsInPort;
+		return this.shipsId.size();
 	}
 	
 	public int[] getPortPosition() {
